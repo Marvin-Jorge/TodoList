aaccf48021d4f12e301a9f20358b142f82c44ce9
Marvin-Jorge <marvinjorge1@gmail.com>
2022-04-26 00:32:49 -0100

    estilisando todo feito

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import './TodoForm.css';
 function TodoForm() {
   return (
     <div className="Todo-App">
+        <h1>Todo List App</h1>
     <form>
       <input type='text'/>
       <button type='onSubmit'>Adicionar Todo</button>
