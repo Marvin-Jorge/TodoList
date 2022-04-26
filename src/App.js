9d3738f4790f56a57adcf9fcd0fb8c9fad9cb395
Marvin-Jorge <marvinjorge1@gmail.com>
2022-04-26 00:14:04 -0100

    mudando app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import TodoForm from "./components/TodoForm";
 function App() {
   return (
     <div className="App">
-      
+      <TodoForm/>
         
     </div>
   );
---
+++ src/components/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TodoForm() {
+  return (
+    <div className="Todo-App">
+    <form>
+      <input type='text'/>
+      <button type='onSubmit'>Adicionar Todo</button>
+    </form>
+    </div>
+  )
+}
+
+export default TodoForm
