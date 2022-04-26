c0bb2c837983d63696c02c44279a67cfb377f535
Marvin-Jorge <marvinjorge1@gmail.com>
2022-04-26 00:51:43 -0100

    funcao handle

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './TodoForm.css';
+import { useState } from 'react';
 function TodoForm() {
+    const [todos,setTodos]=useState([])//Vetor para guardar informacoes//
+  const[todo,setTodo]=useState("")
+  function handleSubmit(e){
+    e.preventDefault()
+    const newTodo={
+        id:new Date().getTime(),
+        text: todo,
+        completed:false,
+        
+        
+      }
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
+  }
   return (
     <div className="Todo-App">
         <h1>Todo List App</h1>
-    <form>
+    <form onSubmit={handleSubmit}>
+    
       <input type='text'/>
       <button type='onSubmit'>Adicionar Todo</button>
     </form>
